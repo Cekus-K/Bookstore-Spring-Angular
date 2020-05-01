484635426c18b6a430dbc4e2f85eeb87ed1618fb
Kamil <cekuskamil@gmail.com>
2020-05-01 19:33:14 +0200

    completed feature - master detail page for individual book

--- bookstore-client-angular/src/app/app.module.ts
+++ bookstore-client-angular/src/app/app.module.ts
@@ -7,10 +7,12 @@ import {AppComponent} from './app.component';
 import {BookListComponent} from './components/book-list/book-list.component';
 import {BookService} from './services/book.service';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
-import { BookCategoryComponent } from './components/book-category/book-category.component';
-import { SearchComponent } from './components/search/search.component';
+import {BookCategoryComponent} from './components/book-category/book-category.component';
+import {SearchComponent} from './components/search/search.component';
+import {BookDetailsComponent} from './components/book-details/book-details.component';
 
 const routes: Routes = [
+  {path: 'books/:id', component: BookDetailsComponent},
   {path: 'books', component: BookListComponent},
   {path: 'search/:keyword', component: BookListComponent},
   {path: 'category/:id', component: BookListComponent},
@@ -24,7 +26,8 @@ const routes: Routes = [
     BookListComponent,
     PageNotFoundComponent,
     BookCategoryComponent,
-    SearchComponent
+    SearchComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
